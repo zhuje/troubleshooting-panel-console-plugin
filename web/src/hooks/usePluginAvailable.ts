2119d9b9bd2ba61082b016839a52434fb7639a21
PeterYurkovich <pyurkovi@redhat.com>
2024-06-10 10:23:00 -0400

    Prevent extra Korrel8r API calls

--- web/src/hooks/usePluginAvailable.ts
+++ web/src/hooks/usePluginAvailable.ts
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { useBoolean } from './useBoolean';
 
-export const usePluginAvailable = (pluginName: string): boolean => {
+export const usePluginAvailable = (pluginName: string): [boolean, boolean] => {
   const [isPluginAvailable, togglePluginAvailable] = useBoolean(false);
+  const [loading, toggleLoading] = useBoolean(true);
 
   React.useEffect(() => {
-    fetch(`/api/plugins/${pluginName}/plugin-manifest.json`).then(
-      (response) => response.status === 200 && togglePluginAvailable(),
-    );
-  }, [togglePluginAvailable, pluginName]);
+    fetch(`/api/plugins/${pluginName}/plugin-manifest.json`)
+      .then((response) => {
+        return response.status === 200 && togglePluginAvailable();
+      })
+      .finally(toggleLoading);
+  }, [togglePluginAvailable, pluginName, toggleLoading]);
 
-  return isPluginAvailable;
+  return [isPluginAvailable, loading];
 };
