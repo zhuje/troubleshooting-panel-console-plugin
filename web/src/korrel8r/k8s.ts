8ce6a801c3058bda6d5e972ca34f4dd6b41f3b8c
Alan Conway <aconway@redhat.com>
2024-08-14 22:34:04 -0400

    fix #45: K8sNode conversions URL to/from query
    - Label search querys
    - Use of apiVersion strings 'group/version' vs GroupVersionKind {group, version, kind}
    - Unit tests for a range of querys/URLs
    - Move k8s integration tests into unit tests

--- web/src/korrel8r/k8s.ts
+++ web/src/korrel8r/k8s.ts
@@ -6,10 +6,29 @@ type GroupVersionKind = {
   version?: string;
   kind: string;
 };
+
 const eventGroupVersionKind: GroupVersionKind = {
-  version: 'v1',
   kind: 'Event',
+  version: 'v1',
 };
+
+type QueryData = {
+  name?: string;
+  namespace?: string;
+  labels?: { [key: string]: string };
+  fields?: { [key: string]: string };
+};
+
+type Model = {
+  kind: string;
+  apiVersion: string; // apiVersion is vN or group/vN
+  path: string;
+  verbs: string[];
+};
+
+const pathRegex = new RegExp(
+  '(?:k8s|search)/(?:(?:ns/([^/]+))|cluster|all-namespaces)(?:/([^/]+)(?:/([^/]+))?)?(/events)?/?$',
+);
 
 export class K8sNode extends Korrel8rNode {
   domain: Korrel8rDomain = Korrel8rDomain.Alert;
@@ -23,144 +42,86 @@ export class K8sNode extends Korrel8rNode {
   }
 
   static fromURL(url: string): Korrel8rNode {
-    const urlObject = new URL('http://domain' + url);
-    const path = urlObject.pathname;
-    if (!path) throw new NodeError('Expected url to contain path');
-    const regex =
-      // eslint-disable-next-line no-useless-escape
-      `(?:k8s|\/search)(?:(?:\/ns\/([^/]+))|\/cluster|` +
-      // eslint-disable-next-line no-useless-escape
-      `\/all-namespaces)(?:\/([^/]+)(?:\/([^/]+)(\/events)?)?)?\/?$`;
-    const [, namespace, resource, name, events] = url.toString().match(regex);
-    const eventsExist = !!events;
+    const urlObject = new URL(url, 'http://domain');
+    const params = new URLSearchParams(urlObject.search);
+    const [, namespace, resource, name, events] = urlObject.pathname.match(pathRegex);
+    const groupVersionKind = this.getGroupVersionKind(resource, params.get('kind'));
 
-    const urlQuerySegment = urlObject.search;
-
-    const urlSearchParams = new URLSearchParams(urlQuerySegment);
-    let groupVersionKind: GroupVersionKind;
-    if (resource.includes('~')) {
-      groupVersionKind = K8sNode.parseGroupVersionKind(resource);
-    } else if (resource !== '') {
-      groupVersionKind = {
-        kind: resource,
-      };
-    } else {
-      const kind = urlSearchParams.get('kind');
-      if (!kind) throw new NodeError('Expected more than 0 relevant query parameters');
-      groupVersionKind = { kind };
-    }
-    if (!groupVersionKind.version) {
-      const resources = getCachedResources();
-      if (!resources) {
-        throw new NodeError(resources);
-      }
-      const models = resources.models.filter(
-        (model) => model.path === resource && model.verbs.includes('watch'),
-      );
-      if (!models || models.length !== 1) {
-        throw new NodeError('missing model');
-      }
-      const model = models[0];
-      groupVersionKind = {
-        kind: model.kind,
-        version: model.apiVersion,
-        group: model.apiGroup,
-      };
-    }
-
-    if (eventsExist) {
-      const className =
-        `k8s:${eventGroupVersionKind.kind}.` +
-        `${eventGroupVersionKind.version}.` +
-        `${eventGroupVersionKind.group ?? ''}`;
+    if (!groupVersionKind || !groupVersionKind.kind) throw new NodeError(`invalid k8s URL: ${url}`);
+    if (events) {
       const data = {
         fields: {
           'involvedObject.namespace': namespace,
           'involvedObject.name': name,
-          'involvedObject.apiVersion': groupVersionKind.version,
+          'involvedObject.apiVersion': this.apiVersionFor(groupVersionKind),
           'involvedObject.kind': groupVersionKind.kind,
         },
       };
-      const query = `${className}:${JSON.stringify(data).trim()}`;
+      const query = `${K8sNode.classFor(eventGroupVersionKind)}:${JSON.stringify(data)}`;
       return new K8sNode(url, query);
+    } else {
+      const data = {
+        namespace: namespace,
+        name: name,
+        labels: K8sNode.parseSelector(params.get('q')) || undefined,
+      };
+      return new K8sNode(url, `${K8sNode.classFor(groupVersionKind)}:${JSON.stringify(data)}`);
     }
-    const className =
-      `k8s:${groupVersionKind.kind}.` +
-      `${groupVersionKind.version ?? 'v1'}.` +
-      `${groupVersionKind.group ?? ''}`;
-    const query = `${className}:{"namespace":"${namespace}",` + `"name":"${name}"}`;
-    return new K8sNode(url, query);
   }
 
   static fromQuery(query: string): Korrel8rNode {
-    if (!query.startsWith('k8s:')) throw new NodeError('Expected query to start with k8s:');
-    const queryAfterDomain = query.substring('k8s:'.length);
-    if (!queryAfterDomain) throw new NodeError('Expected query to contain class');
-    const groupVersionKindString = queryAfterDomain.split(':').at(0);
-    const groupVersionKindSplit = groupVersionKindString.split('.');
-    let groupVersionKind = {
-      kind: groupVersionKindSplit[0],
-      version: groupVersionKindSplit[1],
-      group: groupVersionKindSplit[2],
-    };
-    const resources = getCachedResources();
-    if (!resources) {
-      throw new NodeError('Unable to retrieve resources');
+    const [, domain, clazz, dataStr] = query.match(/^([^:]+):([^:]+):(.+)$/) ?? [];
+    if (domain != 'k8s') throw new NodeError(`invalid k8s query: ${query}`);
+    const [, kind, version, group] = clazz.match(/^([^.]+)(?:\.([^.]*)(?:\.(.*))?)?$/) ?? [];
+    if (!kind) throw new NodeError(`invalid k8s class: ${clazz}`);
+    let gvk: GroupVersionKind = { kind: kind, version: version || 'v1', group: group || undefined };
+
+    let data: QueryData;
+    try {
+      data = JSON.parse(dataStr) as QueryData;
+    } catch (e) {
+      throw new NodeError(`invalid k8s query data: ${dataStr}: ${e}`);
     }
-    const queryAfterClass = queryAfterDomain.substring(groupVersionKindString.length + 1);
-    if (!queryAfterClass || queryAfterClass === '{}')
-      throw new NodeError('Expected more than 0 relevant query parameters');
 
-    const korrel8rQuery = JSON.parse(queryAfterClass);
-
-    let models = resources.models;
-    if (groupVersionKind.kind === eventGroupVersionKind.kind) {
-      models = models.filter(
-        (model) =>
-          model.kind === korrel8rQuery.fields['involvedObject.kind'] &&
-          model.verbs.includes('watch'),
+    let namespace = '',
+      name = '',
+      events = '';
+    if (gvk.kind == eventGroupVersionKind.kind && !gvk.group) {
+      events = '/events';
+      // Focus on the events involved object rather than the event.
+      gvk = this.makeGroupVersionKind(
+        data.fields['involvedObject.apiVersion'],
+        data.fields['involvedObject.kind'],
       );
+      namespace = data.fields['involvedObject.namespace'] || '';
+      name = data.fields['involvedObject.name'] || '';
     } else {
-      models = models.filter(
-        (model) => model.kind === groupVersionKind.kind && model.verbs.includes('watch'),
-      );
+      namespace = data.namespace || '';
+      name = data.name || '';
     }
 
-    if (!models) {
-      throw new NodeError('Unable to find a matching model');
-    }
-    const model = models[0];
-    if (!groupVersionKind.version) {
-      groupVersionKind = {
-        kind: model.kind,
-        version: model.apiVersion,
-        group: model.apiGroup,
-      };
-    }
-    const path = model.path;
+    // Prepare parts of the URL
+    const [gvk2, resource] = this.findKind(gvk); // Fill out partial GVK
+    gvk = gvk2;
+    const nsPath = namespace ? `ns/${namespace}` : 'all-namespaces';
+    const selector = Object.keys(data.labels || {})
+      .map((k) => `${k}=${data.labels[k]}`)
+      .join(',');
+    const qParam = selector ? `q=${encodeURIComponent(selector)}` : '';
 
-    if (
-      groupVersionKind.kind === eventGroupVersionKind.kind &&
-      groupVersionKind.version === eventGroupVersionKind.version
-    ) {
-      let url = `k8s`;
-      if (korrel8rQuery.fields['involvedObject.namespace']) {
-        url = `${url}/ns/${korrel8rQuery.fields['involvedObject.namespace']}`;
-      }
-      url = `${url}/${path}`;
-      if (korrel8rQuery.fields['involvedObject.name']) {
-        url = `${url}/${korrel8rQuery.fields['involvedObject.name']}`;
-      }
-      url = url.concat('/events');
-      return new K8sNode(url, query);
-    }
-    let url = `k8s`;
-    if (korrel8rQuery.namespace) {
-      url = `${url}/ns/${korrel8rQuery.namespace}`;
-    }
-    url = `${url}/${path}`;
-    if (korrel8rQuery.name) {
-      url = `${url}/${korrel8rQuery.name}`;
+    let url: string;
+    if (!name && !namespace && qParam) {
+      // Search URL
+      url =
+        `search/${nsPath}?${qParam}&kind=` +
+        `${gvk.group || 'core'}~` +
+        `${gvk.version || undefined}~` +
+        `${gvk.kind}`;
+    } else {
+      // Resource URL
+      url = `k8s/${nsPath}/${resource}${name ? `/${name}` : ''}${
+        qParam ? `?${qParam}` : ''
+      }${events}`;
     }
     return new K8sNode(url, query);
   }
@@ -173,16 +134,72 @@ export class K8sNode extends Korrel8rNode {
     return this.query;
   }
 
-  private static parseGroupVersionKind(groupVersionKindString: string): GroupVersionKind {
-    const groupVersionKindArray = groupVersionKindString.split('~');
-    if (groupVersionKindArray.length === 3) {
-      return {
-        group: groupVersionKindArray[0],
-        version: groupVersionKindArray[1],
-        kind: groupVersionKindArray[2],
-      };
-    } else {
-      return { kind: groupVersionKindString };
+  // Return a GVK for the resource (path) or throw an error
+  private static getGroupVersionKind(resource: string, kind: string): GroupVersionKind {
+    if (resource && !resource.includes('~')) {
+      // Try the resource as a straight resource name
+      if (resource === 'projects') resource = 'namespaces';
+      return this.findResource(resource);
     }
+    if (resource || kind) {
+      // Either kind or resource may be a g~v~k string.
+      const parts = (resource || kind).split('~');
+      if (parts.length !== 3 || !parts[2]) throw new NodeError('invalid k8s URL');
+      const gvk: GroupVersionKind = {
+        group: !parts[0] || parts[0] === 'core' ? undefined : parts[0],
+        version: parts[1] || undefined,
+        kind: parts[2],
+      };
+      return this.findKind(gvk)[0]; // Fill it out
+    }
+    throw new NodeError('invalid k8s URL');
+  }
+
+  // parseSelector parses a selector string as a query map.
+  private static parseSelector(selector: string): { [key: string]: string } {
+    if (!selector) return undefined;
+    const labels: { [key: string]: string } = {};
+    selector.split(',').forEach((pair: string) => {
+      const [key, value] = pair.split(/=(.*)/);
+      labels[key] = value;
+    });
+    return labels || undefined;
+  }
+
+  // findResource returns the GroupVersionKind for a resource name (path component)
+  private static findResource(resource: string): GroupVersionKind {
+    const model = getCachedResources()?.models.find(
+      (m: Model) => m.path === resource && m.verbs.includes('watch'),
+    );
+    if (!model) throw new NodeError(`k8s resource not recognized: ${resource} `);
+    return K8sNode.makeGroupVersionKind(model.apiVersion, model.kind);
+  }
+
+  // Returns the model matching a gvk or throws an error.
+  private static findKind(gvk: GroupVersionKind): [fullGVK: GroupVersionKind, resource: string] {
+    const apiVersionMatch = `${gvk.group ? `${gvk.group}/` : ''}${gvk.version || ''}`;
+    const model = getCachedResources()?.models.find(
+      (m: Model) =>
+        m.kind === gvk.kind && m.verbs.includes('watch') && m.apiVersion.match(apiVersionMatch),
+    );
+    if (!model) throw new NodeError(`k8s kind not recognized: ${JSON.stringify(gvk)} `);
+    return [this.makeGroupVersionKind(model.apiVersion, model.kind), model.path];
+  }
+
+  // makeGroupVersionKind from a kind and an apiVersion.
+  private static makeGroupVersionKind(apiVersion: string, kind: string): GroupVersionKind {
+    const parts = apiVersion.split('/');
+    if (parts.length == 1) return { version: parts[0] || undefined, kind: kind };
+    if (parts.length == 2)
+      return { group: parts[0] || undefined, version: parts[1] || undefined, kind: kind };
+    throw new NodeError('invalid k8s apiVersion: ${apiVersion}');
+  }
+
+  private static apiVersionFor(gvk: GroupVersionKind): string {
+    return `${gvk.group ? `${gvk.group}/` : ''}${gvk.version || 'v1'}`;
+  }
+
+  private static classFor(gvk: GroupVersionKind): string {
+    return `k8s:${gvk.kind}.${gvk.version || 'v1'}.${gvk.group || ''}`;
   }
 }
