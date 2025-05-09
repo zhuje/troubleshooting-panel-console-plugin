18c2645ff3552e2d18e7198342afbd1aa68c443e
Alan Conway <aconway@redhat.com>
2025-05-09 17:05:03 -0400

    refactor: move graph logic to korrel8r/types, separate from UI

--- web/src/components/topology/Korrel8rTopology.tsx
+++ web/src/components/topology/Korrel8rTopology.tsx
@@ -25,13 +25,11 @@ import {
   withSelection,
 } from '@patternfly/react-topology';
 import * as React from 'react';
-import { TFunction, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom-v5-compat';
-import { useLocationQuery } from '../../hooks/useLocationQuery';
 import { allDomains } from '../../korrel8r/all-domains';
-import * as api from '../../korrel8r/client';
-import { Node as Korrel8rNode, QueryRef } from '../../korrel8r/types';
+import * as korrel8r from '../../korrel8r/types';
 import { Search, SearchType } from '../../redux-actions';
 import { State } from '../../redux-reducers';
 import './korrel8rtopology.css';
@@ -41,24 +39,6 @@ interface Korrel8rTopologyNodeProps {
   onSelect: () => void;
   selected: boolean;
 }
-
-type GraphNode = {
-  id: string;
-  type: string;
-  label: string;
-  width: number;
-  height: number;
-  shape: NodeShape;
-  data: Korrel8rNode;
-};
-
-type GraphEdge = {
-  id: string;
-  type: string;
-  source: string;
-  target: string;
-  edgeStyle: EdgeStyle;
-};
 
 export enum NodeError {
   invalid = 'invalid',
@@ -114,135 +94,90 @@ const baselineComponentFactory: ComponentFactory = (kind: ModelKind, type: strin
 const NODE_SHAPE = NodeShape.ellipse;
 const NODE_DIAMETER = 75;
 
-const getNodesFromResponse = (
-  graph: api.Graph,
-  loggingAvailable: boolean,
-  netobserveAvailable: boolean,
-  t: TFunction,
-  persistedSearch: Search,
-): Array<GraphNode> => {
-  const nodes: Array<GraphNode> = [];
-  graph.nodes?.forEach((graphNode: api.Node) => {
-    const node = new Korrel8rNode(graphNode, allDomains, persistedSearch.constraint);
-    if (node.error) {
-      // eslint-disable-next-line no-console
-      console.error(node.error);
-      node.error = t('Unable to find Console Link');
-    } else if (node.class.domain === 'log' && !loggingAvailable) {
-      node.error = t('Logging Plugin Disabled');
-    } else if (node.class.domain === 'netflow' && !netobserveAvailable) {
-      node.error = t('Netflow Plugin Disabled');
-    }
-    nodes.push({
-      id: node.classStr, // Original class string is always unique in a graph.
-      type: 'node',
-      label: `${nodeLabel(node)} (${node.count})`,
-      width: NODE_DIAMETER,
-      height: NODE_DIAMETER,
-      shape: NODE_SHAPE,
-      data: node,
-    });
-  });
-  return nodes;
-};
-
-const nodeLabel = (node: Korrel8rNode): string => {
+const nodeLabel = (node: korrel8r.Node): string => {
   const c = node.class;
-  switch (c?.domain) {
-    case 'k8s':
-      return c?.name?.split('.')?.[0] || node.classStr;
-    case 'log':
-      return c?.name ? capitalize(c.name) + ' Logs' : node.classStr;
-    default:
-      return c?.domain ? capitalize(c.domain) : node.classStr;
-  }
+  if (!c) return `[${node.api.class}]`; // Original un-parsed class name.
+  if (c.domain === c.name) return capitalize(c.domain);
+  let name = c.name;
+  if (c.domain === 'k8s') name = c.name.match(/^[^.]+/)?.[0] || name; // Kind without version
+  return `${capitalize(c.domain)} ${capitalize(name)} `;
 };
 
-const capitalize = (s: string) => {
-  return s ? s[0].toUpperCase() + s.slice(1) : '';
-};
-
-const getEdgesFromResponse = (graph: api.Graph): Array<GraphEdge> => {
-  const edges: Array<GraphEdge> = [];
-
-  graph.edges?.forEach((edge: api.Edge) => {
-    const start = graph.nodes.find((node: api.Node) => node.class === edge.start);
-    const goal = graph.nodes.find((node: api.Node) => node.class === edge.goal);
-    if (!start || !goal) return;
-    edges.push({
-      id: `edge-${edge.start}-${edge.goal}`,
-      type: 'edge',
-      source: edge.start,
-      target: edge.goal,
-      edgeStyle: EdgeStyle.default,
-    });
-  });
-  return edges;
-};
+const capitalize = (s: string) => (s ? s[0].toUpperCase() + s.slice(1) : '');
 
 export const Korrel8rTopology: React.FC<{
-  graph: api.Graph;
+  graph: korrel8r.Graph;
   loggingAvailable: boolean;
   netobserveAvailable: boolean;
   setSearch: (search: Search) => void;
 }> = ({ graph, loggingAvailable, netobserveAvailable, setSearch }) => {
   const { t } = useTranslation('plugin__troubleshooting-panel-console-plugin');
-  const locationQuery = useLocationQuery();
   const navigate = useNavigate();
-  const persistedSearch = useSelector((state: State) => {
-    return state.plugins?.tp?.get('persistedSearch');
-  }) as Search;
+  const persistedSearch = useSelector((state: State) =>
+    state.plugins?.tp?.get('persistedSearch'),
+  ) as Search;
 
   const nodes = React.useMemo(
-    () => getNodesFromResponse(graph, loggingAvailable, netobserveAvailable, t, persistedSearch),
-    [graph, loggingAvailable, netobserveAvailable, t, persistedSearch],
+    () =>
+      graph.nodes.map((node: korrel8r.Node) => {
+        if (node.error) {
+          // eslint-disable-next-line no-console
+          console.error(node.error);
+          node.error = t('Unable to find Console Link');
+        } else if (node.class.domain === 'log' && !loggingAvailable) {
+          node.error = t('Logging Plugin Disabled');
+        } else if (node.class.domain === 'netflow' && !netobserveAvailable) {
+          node.error = t('Netflow Plugin Disabled');
+        }
+        return {
+          id: node.id,
+          type: 'node',
+          label: `${nodeLabel(node)} (${node.api.count})`,
+          width: NODE_DIAMETER,
+          height: NODE_DIAMETER,
+          shape: NODE_SHAPE,
+          data: node,
+        };
+      }),
+    [graph, loggingAvailable, netobserveAvailable, t],
   );
 
-  const edges = React.useMemo(() => getEdgesFromResponse(graph), [graph]);
-
-  const selectedIds = React.useMemo(() => {
-    return nodes
-      .filter((node) => {
-        try {
-          // This is less efficient, but there are certain plugins which add query params
-          // to the URL that we don't want to match on
-          // TODO - is this still necessary?
-          return node.data.queries.find((qr: QueryRef) => qr.query == locationQuery);
-        } catch (e) {
-          return false;
-        }
-      })
-      .map((node) => node.id);
-  }, [nodes, locationQuery]);
+  const edges = React.useMemo(
+    () =>
+      graph.edges.map((edge: korrel8r.Edge) => {
+        return {
+          id: `edge:${edge.start.id}-${edge.goal.id}`,
+          type: 'edge',
+          source: edge.start.id,
+          target: edge.goal.id,
+          edgeStyle: EdgeStyle.default,
+        };
+      }),
+    [graph],
+  );
 
   const selectionAction = React.useCallback(
     (selected: Array<string>) => {
-      const newlySelectedNode = selected.find(
-        (eachSelection) => !selectedIds.includes(eachSelection),
-      );
-      if (!newlySelectedNode) {
-        return;
+      const id = selected?.[0]; // Select only one at a time.
+      const node = graph.node(id);
+      if (!node || node.error) return;
+      const qc = node.queries?.[0];
+      try {
+        const link = allDomains.queryToLink(qc.query); // FIXME error
+        setSearch({
+          queryStr: qc.query.toString(),
+          type: SearchType.Neighbour,
+          depth: 3,
+          goal: null,
+          constraint: persistedSearch.constraint,
+        });
+        navigate('/' + link);
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error('queryToLink failed: ', e);
       }
-      const node = nodes.find((node) => node.id.startsWith(newlySelectedNode));
-      if (!node || node.data.error) {
-        return;
-      }
-      const korrel8rNode = node?.data;
-      if (!korrel8rNode) {
-        return;
-      }
-      const qc = node.data.queries?.[0];
-      if (!qc?.link) return;
-      setSearch({
-        queryStr: qc.query.toString(),
-        type: SearchType.Neighbour,
-        depth: 3,
-        goal: null,
-        constraint: persistedSearch.constraint,
-      });
-      navigate('/' + qc.link.toString());
     },
-    [navigate, nodes, selectedIds, setSearch, persistedSearch],
+    [graph, navigate, setSearch, persistedSearch],
   );
 
   const controller = React.useMemo(() => {
@@ -298,7 +233,7 @@ export const Korrel8rTopology: React.FC<{
       }
     >
       <VisualizationProvider controller={eventController}>
-        <VisualizationSurface state={{ selectedIds }} />
+        <VisualizationSurface />
       </VisualizationProvider>
     </TopologyView>
   );
